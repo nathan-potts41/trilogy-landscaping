dc6a2d4b144306d7c5701a3e1ff878a03fa8ef36
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-15 10:41:09 -0600

    added hero img and nav

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// activates img carousel
+$(document).ready(function () {
+    $('.carousel').carousel({
+        dist: -200,
+        numVisible: 3,
+        padding: 10,
+    });
+});
+
+//activated img popout
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
